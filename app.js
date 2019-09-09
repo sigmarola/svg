6b2294d862662593188de98903b2c91580181152
sigmarola <42818933+sigmarola@users.noreply.github.com>
2019-09-09 00:32:18 +0000

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ let tl = anime.timeline({
 tl.add({
     targets: ".an1",
     translateX:[ {value:"-20",duration:400},
-                {value:"20",duration:200}],
+                {value:"40",duration:200}],
     translateY: [ {value:"-20",duration:400},
-                {value:"-13",duration:200}],
+                {value:"-20",duration:200}],
     rotate: "20"
 })
   .add({
